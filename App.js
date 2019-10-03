38e19bc20f941a6f63d398faef6c091b38fce419
Bakhtiyar-Garashov <qarashov787@gmail.com>
2019-10-03 17:33:35 +0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+
+
+
+
+function bulb_off(){
+    document.getElementById('picture').src='images/pic_bulboff.gif';
+}
+
+function bulb_on(){
+    document.getElementById('picture').src='images/pic_bulbon.gif';
+}
